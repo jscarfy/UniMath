94e41d5e59ec5de82c893133018b3022fe4feaf0
jscarfy <35473948+jscarfy@users.noreply.github.com>
2019-06-19 10:03:49 -0700

    Create Primes.v

---
+++ UniMath/NumberTheory/Primes.v
@@ -0,0 +1,13 @@
+(** * Definition of Prime *)
+(** Justin Scarfy. Jun. 2019 *)
+
+Unset Kernel Term Sharing.
+
+Require Export UniMath.Algebra.BinaryOperations.
+Require Import UniMath.Foundations.Propositions.
+
+Require Import UniMath.MoreFoundations.Tactics.
+Require Import UniMath.MoreFoundations.DecidablePropositions.
+
+(** ** Additionals theorems about relations *)
+
